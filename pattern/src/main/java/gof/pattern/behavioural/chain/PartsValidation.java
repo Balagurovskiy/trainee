d1d14932446eee180bca559f76e428316c96fad2
Balagurovskiy <balagurovskiyos@gmail.com>
2021-03-11 10:38:19 +0200

    garbage deletion; beh/chain/PartsValidator fix

--- pattern/src/main/java/gof/pattern/behavioural/chain/PartsValidation.java
+++ pattern/src/main/java/gof/pattern/behavioural/chain/PartsValidation.java
@@ -4,13 +4,6 @@ public class PartsValidation extends Production{
 
 	@Override
 	public boolean executeTask(Car car) {
-//		car.getParts().stream().filter(p -> CarExaminator.validatePart(p));
-//		if(CarExaminator.validateAssembly()) {
-//			System.out.println("ok");
-//		} else {
-//			System.out.println("not ok");
-//			System.out.println(CarExaminator.wheelCount);
-//		}
 		int engineCount = 0;
 		int wheelCount = 0;
 		for (Part p : car.getParts()) {
@@ -27,30 +20,4 @@ public class PartsValidation extends Production{
 		}
 		return forward(car);
 	}
-	
-	
-//	private static class CarExaminator{
-//		
-//		private static int engineCount = 0;
-//		private static int wheelCount = 0;
-//		
-//		public static boolean validatePart(Part part) {
-//			
-//			if(part instanceof Engine) {
-//				engineCount++;
-//			} else if(part instanceof Wheel) {
-//				wheelCount++;
-//			} else {
-//				return false;
-//			}
-//			return true;
-//		}
-//		
-//		public static boolean validateAssembly() {
-//			if(engineCount == 1 && wheelCount == 4) {
-//				return true;
-//			}
-//			return false;
-//		}
-//	}
 }
--- pattern/src/main/java/gof/pattern/creational/factory/Sedan.java
+++ pattern/src/main/java/gof/pattern/creational/factory/Sedan.java
@@ -2,6 +2,5 @@ package gof.pattern.creational.factory;
 
 public class Sedan implements Car {
 	public Sedan() {
-//		System.out.println("Sedan created.");
 	}
 }
