12d868a6f4a7529df0438eeb8adb670869457110
Balagurovskiy <balagurovskiyos@gmail.com>
2021-03-16 11:15:45 +0200

    hash map : resize, rehash, remove, clear, contains

--- collections/src/main/java/my/collections/map/TraineeMap.java
+++ collections/src/main/java/my/collections/map/TraineeMap.java
@@ -2,6 +2,7 @@ package my.collections.map;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class TraineeMap<K, V> implements Map<K, V>{
@@ -31,8 +32,7 @@ public class TraineeMap<K, V> implements Map<K, V>{
 	 */
 	@Override
 	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return false;
+		return !Objects.isNull(get(key));
 	}
 	/*
 	 * Returns {@code true} if this map maps one or more keys to the
@@ -40,7 +40,19 @@ public class TraineeMap<K, V> implements Map<K, V>{
 	 */
 	@Override
 	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < capacity; i++) {
+			TraineeEntry<K, V> bucket = this.buckets[ i ];
+			while(bucket != null) {
+				if (Objects.isNull(bucket.getValue())) {
+					if(Objects.isNull(bucket.getValue())) {
+						return true;
+					}
+				}else if (bucket.getValue().equals(value)) {
+					return true;
+				}
+	    		bucket = bucket.getNext();
+	    	}
+		}
 		return false;
 	}
 
@@ -57,31 +69,57 @@ public class TraineeMap<K, V> implements Map<K, V>{
     	}
 		return null;
 	}
+	
+	/*
+	 * Create hash from key Object
+	 */
 	private int getHash(Object key) {
 		int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
 	}
+	
+	/*
+	 * Calculate bucket id 
+	 */
 	private int getBucketIndex(int hash) {
 		return hash % capacity;
 	}
+	
+	/*
+	 * Resize when reaching the threshold
+	 */
 	private void resize() {
 		if ((size + 1) >= (capacity * LOAD_FACTOR)){
-        	//create new buckets x 2
-        	//put etries to new bucket
-        	//clear old
-        	//redefine field to new
-        }
+			int oldCapasity = capacity;
+			capacity *= 2;
+			TraineeEntry<K,V>[] resizedBuckets = new TraineeEntry[capacity];
+
+			for (int i = 0; i < oldCapasity; i++) {
+				TraineeEntry<K, V> bucket = this.buckets[ i ];
+				while(bucket != null) {
+					put(resizedBuckets, bucket.getKey(), bucket.getValue(), false);
+		    		bucket = bucket.getNext();
+		    	}
+			}
+			this.buckets = resizedBuckets;
+		}
 	}
-	@Override
-	public V put(K key, V value) {
+	/*
+	 * create new bucket or add to existing as linked list.
+	 * @param isNewEntry switch between new entry and rehashing old entries
+	 */
+	private V put(TraineeEntry<K,V>[] b, K key, V value, boolean isNewEntry) {
 		TraineeEntry<K, V> newEntry = new TraineeEntry<K, V>(key, value, null);
+		
 		int index = getBucketIndex( getHash(newEntry.getKey()) );
 
-		TraineeEntry<K, V> bucket = buckets[index];
+		TraineeEntry<K, V> bucket = b[index];
         
         if (bucket == null) {
-            buckets[index] = newEntry;
-            size++;
+            b[index] = newEntry;
+            if (isNewEntry) {
+            	size++;
+            }
         } else {
         	while(bucket.getNext() != null) {
         		if(bucket.getKey().equals(key)) {
@@ -97,15 +135,49 @@ public class TraineeMap<K, V> implements Map<K, V>{
     			return oldValue;
     		} else {
     			bucket.setNext(newEntry);
-			 	size++;
+    			if (isNewEntry) {
+                	size++;
+                }
     		}
         }
 		return null;
 	}
+	@Override
+	public V put(K key, V value) {
+		resize();
+		return put(this.buckets , key, value, true);
+	}
 
 	@Override
 	public V remove(Object key) {
-		// TODO Auto-generated method stub
+		int index = getBucketIndex( getHash(key) );
+		
+		TraineeEntry<K, V> bucket = buckets[index];
+		
+		if ( Objects.isNull(bucket) ){
+			return null;
+		}
+		if(bucket.getKey().equals(key)) {
+			V value = bucket.getValue();
+			if ( Objects.isNull(bucket.getNext()) ){
+				buckets[index] = null;
+			} else {
+				buckets[index] = bucket.getNext();
+			}
+			size--;
+			return value;
+		} else {
+			while(bucket.getNext() != null) {
+	    		if(bucket.getNext().getKey().equals(key)) {
+	    			V value = bucket.getValue();
+	    			bucket.setNext(bucket.getNext().getNext());
+	    			size--;
+	    			return value;
+	    		}
+	    		bucket = bucket.getNext();
+    		}
+		}
+
 		return null;
 	}
 
@@ -116,8 +188,17 @@ public class TraineeMap<K, V> implements Map<K, V>{
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < buckets.length; i++) {
+			TraineeEntry<K, V> bucket = this.buckets[i];
+			while(bucket != null) {
+				buckets[i] = null;
+	    		bucket = bucket.getNext();
+	    	}
+			
+		}
+		size = 0;
 	}
+	
 	@Override
 	public Set<K> keySet() {
 		throw new UnsupportedOperationException();
--- collections/src/test/java/my/collections/map/MapTest.java
+++ collections/src/test/java/my/collections/map/MapTest.java
@@ -1,5 +1,7 @@
 package my.collections.map;
 
+import static org.junit.Assert.fail;
+
 import java.util.Map;
 
 import org.junit.Assert;
@@ -23,5 +25,75 @@ public class MapTest {
 		map.put(111, "SECOND");
 		map.put(6, "THIRD");
 		Assert.assertEquals("THIRD", map.get(6));
+	}
+	@Test
+	public void mapTest_Clear_SizeEquals() {
+		Map<Integer, String> map = new TraineeMap<Integer, String>();
+		map.put(1, "FIRST");
+		map.put(111, "SECOND");
+		map.put(6, "THIRD");
+		map.clear();
+		Assert.assertEquals(0, map.size());
+	}
+	@Test
+	public void mapTest_resize_collision_NoException() {
+		try {
+			Map<String, String> map = new TraineeMap<String, String>();
+			for (Integer i = 0; i < 100; i++) {
+				map.put(String.valueOf(i), String.valueOf(i));
+			}
+			for (Integer i = 0; i < 10; i++) {
+				map.put(String.valueOf(i), String.valueOf(i));
+			}
+		}catch (Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void mapTest_Remove_ZeroSize() {
+		Map<String, String> map = new TraineeMap<String, String>();
+		for (Integer i = 0; i < 1000; i++) {
+			map.put(String.valueOf(i), String.valueOf(i));
+		}
+		
+		for (Integer i = 0; i < 1000; i++) {
+			map.remove(String.valueOf(i));
+		}
+		Assert.assertEquals(0, map.size());
+	}
+	
+	@Test
+	public void mapTest_containsKey_True() {
+		Map<String, String> map = new TraineeMap<String, String>();
+		for (Integer i = 0; i < 1000; i++) {
+			map.put(String.valueOf(i), String.valueOf(i));
+		}
+		Assert.assertTrue(map.containsKey("777"));
+	}
+	
+	@Test
+	public void mapTest_containsKey_False() {
+		Map<String, String> map = new TraineeMap<String, String>();
+		for (Integer i = 0; i < 1000; i++) {
+			map.put(String.valueOf(i), String.valueOf(i));
+		}
+		Assert.assertFalse(map.containsKey("1111"));
+	}
+	@Test
+	public void mapTest_containsValue_True() {
+		Map<String, String> map = new TraineeMap<String, String>();
+		for (Integer i = 0; i < 1000; i++) {
+			map.put(String.valueOf(i), String.valueOf(i));
+		}
+		Assert.assertTrue(map.containsValue("777"));
+	}
+	@Test
+	public void mapTest_containsValue_False() {
+		Map<String, String> map = new TraineeMap<String, String>();
+		for (Integer i = 0; i < 1000; i++) {
+			map.put(String.valueOf(i), String.valueOf(i));
+		}
+		Assert.assertFalse(map.containsValue("1111"));
 	}
 }
